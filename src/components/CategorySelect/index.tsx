9f2d6319664e8014adb168c7dc4ee3fc916b6fcb
vbuarque <69692614+vbuarque@users.noreply.github.com>
2021-12-15 19:57:45 -0300

    Home page and category components

---
+++ src/components/CategorySelect/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ScrollView } from "react-native";
+
+import {styles} from "./styles";
+import { categories } from "../../utils/categories";
+
+export function CategorySelect(){
+    return(
+        <ScrollView horizontal style={styles.container} showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{paddingRight: 40}}
+        >
+            {
+                categories.map(category => (
+                    <Category 
+                    
+                    />
+                ))
+            }
+        </ScrollView>
+    );
+}
--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,29 +1,36 @@
 import React from "react";
-import { View, Text, Image, StatusBar } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
 
-import { ButtonIcon } from '../../components/ButtonIcon'
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
+import { ButtonIcon } from '../../components/ButtonIcon'
+
 export function SignIn(){
+  const navigation = useNavigation();
+
+  function handleSignin() {
+    navigation.navigate('Home');
+  }
+  
   return(
     <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
       <Image source={IllustrationImg} style={styles.image} resizeMode="stretch"/>
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`}
-          facilmente
+          Conecte-se {'\n'}
+          e organize suas {'\n'}
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
+          Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos
         </Text>
 
-        <ButtonIcon title="Entre com Discord" activeOpacity={0.7}/>
+        <ButtonIcon onPress={handleSignin} title="Entre com Discord" />
       </View>
     </View>
   )
